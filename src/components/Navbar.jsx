9e18b29e865952eb7d7a6340f0ef07947ed85251
MortezaSaghi <morteza66saghi@gmail.com>
2024-01-20 00:17:32 +0100

    created character detail section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {HeartIcon} from "@heroicons/react/24/outline"
 export default function Navbar() {
   return (
     <nav className="navbar">
-        <div className="navbar__logo">Logo</div>
+        <div className="navbar__logo"><img src="./../../public/Rick_and_Morty.png" alt="" /></div>
         <input type="text" className="text-field" placeholder="Search..."/>
         <div className="navbar__result">Found X characters</div>
         <button className="heart">
