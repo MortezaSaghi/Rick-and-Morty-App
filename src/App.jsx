b6aca839627aa61a8caaabc4d9c1dfeffbb4a94c
MortezaSaghi <morteza66saghi@gmail.com>
2024-01-24 13:52:33 +0100

    Local Storage Favourites

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,15 @@ import Navbar from "./components/Navbar";
 import CharacterList from "./components/CharacterList";
 import ChracterDetail from "./components/ChracterDetail";
 import { Toaster, toast } from "react-hot-toast";
-import Modal from "./components/Modal"
 
 function App() {
   const [characters, setCharacters] = useState([]);
   const [isLoding, setIsLoding] = useState(false);
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
-  const [favourites, setFavourates] = useState([]);
+  const [favourites, setFavourates] = useState(
+    () => JSON.parse(localStorage.getItem("FAVOURITES")) || []
+  );
 
   const handelChangeQuery = (value) => {
     setQuery(value);
@@ -24,38 +25,47 @@ function App() {
     setSelectedId((prevId) => (prevId === id ? null : id));
     // console.log(id);
   };
-  
+
   const isFavourite = favourites.map((fav) => fav.id).includes(selectedId);
 
-  const handelDeleteFavourite=(id)=>{
-    setFavourates((preFavourites)=>preFavourites.filter((item) => item.id !== id));
-  }
+  const handelDeleteFavourite = (id) => {
+    setFavourates((preFavourites) =>
+      preFavourites.filter((item) => item.id !== id)
+    );
+  };
 
   const handelAddFavourites = (char) => {
     if (!isFavourite) {
       setFavourates((preFav) => [...preFav, char]);
     } else {
-      setFavourates((preFavourites)=>preFavourites.filter((fav) => fav.id !== selectedId));
+      setFavourates((preFavourites) =>
+        preFavourites.filter((fav) => fav.id !== selectedId)
+      );
     }
   };
 
+  //------------- SET FAVOURITES ITEM LOCALSTORAGE
+  useEffect(() => {
+    localStorage.setItem("FAVOURITES", JSON.stringify(favourites));
+  }, [favourites]);
+
   //---------- Fechting Data
   useEffect(() => {
-    const controller= new AbortController();
-    const signal=controller.signal;
+    const controller = new AbortController();
+    const signal = controller.signal;
     async function getData() {
       try {
         setIsLoding(true);
         const { data } = await axios.get(
-          `https://rickandmortyapi.com/api/character?name=${query}`,{signal}
+          `https://rickandmortyapi.com/api/character?name=${query}`,
+          { signal }
         );
         setCharacters(data.results.slice(0, 6));
       } catch (err) {
-        if(!axios.isCancel()){
+        if (!axios.isCancel()) {
           setCharacters([]);
           toast.error(err.response.data.error);
         }
-        
       } finally {
         setIsLoding(false);
       }
@@ -66,15 +76,15 @@ function App() {
     }
     getData();
     // ---- clean up fetch api  (re-redder => searchbox)
-    return ()=>{
+    return () => {
       controller.abort();
-    }
+    };
   }, [query]);
 
   return (
     <div className="app">
       <Toaster />
-    
+
       <Navbar
         numOfResult={characters.length}
         query={query}
